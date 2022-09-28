894b7a67b4f2f9be1124a94799efb82259cfe231
ChristoferMendes <christoferluizdsm@gmail.com>
2022-09-28 13:42:13 -0300

    created issues filters

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 import {useParams } from 'react-router-dom'
 import { Loading } from '../../misc/Loading';
 import { api } from '../../services/api';
 // import api from '../../services/api'
 
-import { Container, Owner } from './styles'
+import { BackButton, Container, IssuesList, Owner, PageActions, StateFilter } from './styles'
 
 interface Repository {
     owner?: { avatar_url: string, login: string; }
@@ -12,22 +13,36 @@ interface Repository {
     description?: string;
 }
 
+interface Issues {
+  id: number;
+  user: { avatar_url: string, login: string }
+  title: string;
+  html_url: string;
+  labels: [{id: number, name: string}];
+}
+
 type RepositoryState = Repository;
 
 export const Repository = () => {
   const { repository: repositoryName } = useParams();
   const [repository, setRepository] = useState<RepositoryState>({});
-  const [issues, setIssues] = useState([])
+  const [issues, setIssues] = useState<Issues[]>([])
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [issueState, setIssueState] = useState([
+    {state: 'all', label: 'All', active: true},
+    {state: 'open', label: 'Open', active: false},
+    {state: 'closed', label: 'Closed', active: false}
+  ])
+  const [issueFocus, setIssueFocus] = useState(0);
   
-
   useEffect(() => {
     (async () => { //SELF-INVOKING-FUNCTION
       const [repositoryData, issuesData] = await Promise.all([
         api.get<Repository>(`/repos/${repositoryName}`),
         api.get(`/repos/${repositoryName}/issues`, {
           params: {
-            state: 'open',
+            state: issueState.find(a => a.active)?.state,
             per_page: 5,
           }
         })
@@ -39,21 +54,86 @@ export const Repository = () => {
     })()
   }, [repositoryName])
 
+  useEffect(() => {
+    (async () => {
+      const response = await api.get(`/repos/${repositoryName}/issues`, {
+        params: {
+          state: issueState[issueFocus].state,
+          page,
+          per_page: 5,
+        }
+      })
+
+      setIssues(response.data)
+    })()
+  }, [repositoryName, page, issueState, issueFocus])
+
+  const handlePage = (action: string) => {
+    setPage(action === 'back' ? page - 1 : page + 1)
+  }
+  
+  const handleIssueFilter = (index: number) => {
+    setIssueFocus(index);
+  }
+
   if (loading) {
     return <Loading />
   }
 
   return (
     <Container>
+      <BackButton to={'/'}> 
+        <FaArrowLeft color='#000' size={30} />
+      </BackButton>
+  
        <Owner>
         <img 
           src={repository.owner?.avatar_url} 
           alt={`${repository.owner?.login} avatar`}
         />
-        <h1>{repository.name}</h1>
-        <p>{repository.description}</p>
         <h1>{repositoryName}</h1>
+        <p>{repository.description}</p>
        </Owner>
+
+      <StateFilter active={issueFocus}>
+        {issueState.map((row, index) => (
+          <button 
+          type='button' 
+          key={row.label}
+          onClick={() => handleIssueFilter(index)}>{row.label}</button>
+        ))}
+      </StateFilter>
+        
+      
+       <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={label.id.toString()}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+       </IssuesList>
+
+       <PageActions>
+          <button 
+          type='button' 
+          onClick={() => handlePage('back')}
+          disabled={page < 2}
+          >
+            Previous
+          </button>
+          <button type='button' onClick={() => handlePage('next')}>Next</button>
+       </PageActions>
     </Container>
   )
 }
