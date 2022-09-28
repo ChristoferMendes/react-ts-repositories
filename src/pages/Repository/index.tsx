ce17e5894f75bb5f5be25f1fb36190b380fe9ad8
ChristoferMendes <christoferluizdsm@gmail.com>
2022-09-28 11:33:37 -0300

    repository page created

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from 'react';
+import {useParams } from 'react-router-dom'
+import { Loading } from '../../misc/Loading';
+import { api } from '../../services/api';
+// import api from '../../services/api'
+
+import { Container, Owner } from './styles'
+
+interface Repository {
+    owner?: { avatar_url: string, login: string; }
+    name?: string;
+    description?: string;
+}
+
+type RepositoryState = Repository;
+
 export const Repository = () => {
+  const { repository: repositoryName } = useParams();
+  const [repository, setRepository] = useState<RepositoryState>({});
+  const [issues, setIssues] = useState([])
+  const [loading, setLoading] = useState(true);
+  
+
+  useEffect(() => {
+    (async () => { //SELF-INVOKING-FUNCTION
+      const [repositoryData, issuesData] = await Promise.all([
+        api.get<Repository>(`/repos/${repositoryName}`),
+        api.get(`/repos/${repositoryName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          }
+        })
+      ])
+
+      setRepository(repositoryData.data)
+      setIssues(issuesData.data)
+      setLoading(false);
+    })()
+  }, [repositoryName])
+
+  if (loading) {
+    return <Loading />
+  }
+
   return (
-    <div>
-       <h1>Repository</h1>
-          <h3>dsadas</h3>
-    </div>
+    <Container>
+       <Owner>
+        <img 
+          src={repository.owner?.avatar_url} 
+          alt={`${repository.owner?.login} avatar`}
+        />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+        <h1>{repositoryName}</h1>
+       </Owner>
+    </Container>
   )
 }
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  h1 {
+    color: #fff;
+    text-transform: capitalize;
+  }
+`;
+
+export const Owner = styled.header`
+
+`
